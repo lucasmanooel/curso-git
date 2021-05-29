e2c54fc693f0c557dd5cfb34700b0c4615dd8c9c
Lucas Manoel <lucasmanoelmelo@gmail.com>
2021-05-29 09:49:18 -0300

    Projeto iniciado mostrando uma mensagem ola mundo

---
+++ 001-Primeiro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _001_Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola, mundo!");
+        }
+    }
+}
